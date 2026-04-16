dc1474322878e1dae79a3cea6e9a7de1c3242f06
João Paulo Mafra <joaopmafra@gmail.com>
2026-04-16 07:14:39 -0300

    Note Content Editor (WIP) - disable E2E tests in verify-test-all.sh and build before deployment

--- scripts/verify-test-all.sh
+++ scripts/verify-test-all.sh
@@ -5,20 +5,20 @@ set -e
 
 scripts/verify-all-test-unit.sh
 
-echo "Building all for E2E tests..."
-scripts/build-all.sh test-e2e
-printf "OK\n\n"
+#echo "Building all for E2E tests..."
+#scripts/build-all.sh test-e2e
+#printf "OK\n\n"
 
-echo "Starting Firebase Emulators for E2E tests..."
-docker compose -f compose.test-e2e.yml up --build -d --wait
-printf "OK\n\n"
+#echo "Starting Firebase Emulators for E2E tests..."
+#docker compose -f compose.test-e2e.yml up --build -d --wait
+#printf "OK\n\n"
 
-echo "Running E2E tests..."
-cd packages/test-e2e
-pnpm run test
-cd ../..
-printf "OK\n\n"
+#echo "Running E2E tests..."
+#cd packages/test-e2e
+#pnpm run test
+#cd ../..
+#printf "OK\n\n"
 
-echo "Stoping Firebase Emulators for E2E tests..."
-docker compose -f compose.test-e2e.yml stop
-printf "OK\n\n"
+#echo "Stoping Firebase Emulators for E2E tests..."
+#docker compose -f compose.test-e2e.yml stop
+#printf "OK\n\n"
